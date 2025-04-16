3f36e313b4cea8dd5495d4b8cd3724a023e1bba9
JeanCsantos7 <jean.carlos87678@gmail.com>
2025-04-16 17:29:00 -0300

    refatorandoSwagger

--- swagger.ts
+++ swagger.ts
@@ -1,23 +1,25 @@
 import swaggerJSDoc from "swagger-jsdoc";
 
 const options = {
-  definition: {
-    openapi: "3.0.4",
-    info: {
-      title: "API de agendamento de horários barbearia",
-      description:
-        "API pra realizar agendamento de horários, escolha de serviços e autenticação com base em regras de permissão.",
-      version: "1.0.0",
+    definition: {
+      openapi: "3.0.4", 
+      info: {
+        title: "API de agendamento de horários barbearia", 
+        description:
+          "API para realizar agendamento de horários, escolha de serviços e autenticação com base em regras de permissão.", 
+        version: "1.0.0", 
+      },
     },
     servers: [
       {
-        url: "https://barbearia-backend-six.vercel.app/",
+        url: "https://barbearia-backend-six.vercel.app", 
       },
     ],
-  },
-  apis: ["./routes/*.ts"], 
-};
-
-const swaggerDefinition = swaggerJSDoc(options);
-
-export { swaggerDefinition };
+    //
+    api: ["./routes/*.ts"], 
+  };
+  
+  const swaggerDefinition = swaggerJSDoc(options);
+  
+  export { swaggerDefinition };
+  
