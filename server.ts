cb7ed394157cee6d8129018464c73585dd799891
JeanCsantos7 <jean.carlos87678@gmail.com>
2025-04-16 17:24:50 -0300

    refatorandoServer

--- server.ts
+++ server.ts
@@ -1,14 +1,40 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 import app from "./app";
-import swaggerUi from "swagger-ui-express";
+import dotenv from "dotenv";
 import { swaggerDefinition } from "./swagger";
+
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 const isProd = process.env.NODE_ENV === 'production';
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDefinition));
+
+app.get("/swagger.json", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.send(swaggerDefinition);
+});
+
+
+app.get("/api-docs", (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Documentação da API</title>
+        <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css" />
+      </head>
+      <body>
+        <div id="swagger-ui"></div>
+        <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
+        <script>
+          SwaggerUIBundle({
+            url: "/swagger.json",
+            dom_id: "#swagger-ui"
+          });
+        </script>
+      </body>
+    </html>
+  `);
+});
 
 app.listen(PORT, () => {
   console.log(`🟢 Servidor rodando em: ${isProd ? 'https://barbearia-backend-six.vercel.app' : `http://localhost:${PORT}`}`);
